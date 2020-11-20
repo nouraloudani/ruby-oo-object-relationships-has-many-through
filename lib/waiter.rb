88549af57105aba452322864e7d45a7375a1f845
Noura <contact.nouraloudani@gmail.com>
2020-11-20 01:15:05 -0500

    solution completed

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,9 +14,8 @@ class Waiter
       @@all
     end
     
-
-    def new_meal(waiter, customer, total, tip = 0)
-        Meal.new(self, customer, total, tip)
+    def new_meal(customer, total, tip=0)
+      Meal.new(self, customer, total, tip)
     end
 
     def meals
